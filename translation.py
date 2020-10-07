81f96aac57dfdf883f044a52538675a8669efb67
DigitalJay <pushkareva_lisa@mail.ru>
2020-10-07 23:03:49 +0500

    More examples

--- translation.py
+++ translation.py
@@ -2,4 +2,7 @@ from googletrans import Translator
 def translate(the_text):
     translator = Translator()
     return translator.translate(the_text,dest='ru').text
-print(translate('hello, how are you?'))
+
+print(translate('What have we found? Same old fears'))
+print(translate("Pink Floyd or Avenged Sevenfold"))
+print(translate("July 2019"))
